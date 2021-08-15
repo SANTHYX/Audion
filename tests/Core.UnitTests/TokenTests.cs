8eb83b00bd0de310bc2362115a7ea117ada2f61b
Damian Dorocicz <damiand44444@gmail.com>
2021-08-15 19:12:49 +0200

    tests

---
+++ tests/Core.UnitTests/TokenTests.cs
@@ -0,0 +1,37 @@
+﻿using Core.Domain;
+using Moq;
+using Xunit;
+using FluentAssertions;
+using System;
+
+namespace Core.UnitTests
+{
+    public class TokenTests
+    {
+        private readonly Mock<Token> _tokenMock;
+
+        public TokenTests()
+        {
+            _tokenMock = new();
+        }
+
+        [Fact]
+        public void Is_RevokeToken_change_value_to_false_when_token_is_not_revoked_yet()
+        {
+            var tokenObj = _tokenMock.Object;
+
+            tokenObj.RevokeToken();
+            tokenObj.IsRevoked.Should().Be(true);
+        }
+
+        [Fact]
+        public void Is_RevokeToken_throws_exception_when_token_is_already_revoked()
+        {
+            var tokenObj = _tokenMock.Object;
+
+            tokenObj.RevokeToken();
+            Action revokeTokenMethod = () => tokenObj.RevokeToken();
+            revokeTokenMethod.Should().Throw<Exception>();
+        }
+    }
+}
